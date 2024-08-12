2d73727f71e2d05c09edeebc42ba972db0e27bd1
mvtalan <mvtalan@gmail.com>
2024-08-12 12:20:18 -0600

    with form

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const userRoute = require('./routes/users.js');
+// const ejs = require('ejs');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
-app.set('views', '/views');
+app.set('views', './views');
 
 app.use(express.static('public'));
+app.use(userRoute);
 
 app.listen(PORT, () => {
     console.log(`Conncted on port ${PORT}`);
