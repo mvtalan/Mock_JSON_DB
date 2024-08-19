c96939e7af0659cc35b4ae55685bb88d8a8caa76
mvtalan <mvtalan@gmail.com>
2024-08-19 09:52:30 -0600

    CRUD

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,11 +26,64 @@ async function createUser(req, res) {
         res.send("User added successfully.");
 
     } catch(err) {
-        res.send("Internal server error.");
+        res.status(500).json("Internal server error.");
 
     }
 }
 
+//function for updating user
+async function updateUser(req, res) {
+    try {
+        const data = await readData();
+        const user = data.users.find(user => user.id === parseInt(req.params.id));
+
+        if(user) {
+            user.userName = req.body.new_username || user.userName;
+            user.first_name = req.body.new_first_name || user.first_name;
+            user.email = req.body.new_email || user.email;
+
+            await writeData(data);
+
+            // res.redirect('/');
+            res.send("User updated succesfully.");
+
+        } else {
+            res.status(404).send("User not found.");
+        }
+
+    } catch (err) {
+        res.status(500).json("Internal server error.");
+    }
+}
+
+async function deleteUser(req, res) {
+    try {
+        const data = await readData();
+        const userIndex = data.users.findIndex(user => user.id === parseInt(req.params.id));
+
+        if(userIndex !== -1) {
+            data.users.splice(userIndex, 1);
+            await writeData(data);
+
+            res.send("User deleted succesfully.");
+
+        } else {
+            res.status(404).send('User not found.')
+        }
+
+        res.redirect('/');
+        // data.users.splice(user, 1);
+        // await writeData(data);
+
+        // res.send("User deleted succesfully.");
+
+    } catch (err) {
+        res.status(500).json("Internal server error.");
+    }
+}
+
 module.exports = {
-    createUser
+    createUser,
+    updateUser,
+    deleteUser
 }
